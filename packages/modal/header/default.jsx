18adc1782b945a98b45e3f31b52fb62f62702dcc
Joel Moss <joel@developwithstyle.com>
2022-11-29 09:52:13 +0000

    Added button component

--- packages/modal/header/default.jsx
+++ packages/modal/header/default.jsx
@@ -1,7 +1,7 @@
 // import BackIcon from "assets/images/icons/chevron-left-regular.svg"
 // import CloseIcon from "assets/images/icons/times-regular.svg"
 // import Icon from "components/lib/icon"
-// import Button from "components/lib/html_elements/button"
+import Button from "@lowui/button"
 
 import styles from "./index.module.css"
 
@@ -15,17 +15,17 @@ const DefaultHeader = ({ onClose, title, canClose }) => {
   return (
     <>
       {canClose && (
-        <button className={styles.closeButtonMobile} onClick={onClose} title="Close">
+        <Button className={styles.closeButtonMobile} onClick={onClose} title="Close">
           {/* <Icon icon={<BackIcon />} /> */}
           back
-        </button>
+        </Button>
       )}
       <h2>{title}</h2>
       {canClose && (
-        <button className={styles.closeButtonDesktop} onClick={onClose} title="Close">
+        <Button className={styles.closeButtonDesktop} onClick={onClose} title="Close">
           {/* <Icon icon={<CloseIcon />} /> */}
           close
-        </button>
+        </Button>
       )}
     </>
   )
