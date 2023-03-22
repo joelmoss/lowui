7a7287486bb21a749979f1ea1a1f4202836b1524
Joel Moss <joel@developwithstyle.com>
2023-03-22 22:37:05 +0000

    fix: sheet build

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,17 @@
+/* global __dirname */
+
+const path = require('node:path')
+
 module.exports = {
   plugins: {
     "@developwithstyle/postcss-custom-media": {
-      importFrom: "../../config/custom_media_queries.css",
+      importFrom: "../../config/custom_media_queries.css"
     },
-    "postcss-mixins": {},
-  },
+    "postcss-mixins": {
+      mixinsFiles: [
+        path.join(__dirname, "lib", "mixins", "*.mixin.css"),
+        path.join(__dirname, "packages", "**/*.mixin.css")
+      ]
+    }
+  }
 }
